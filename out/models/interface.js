9845e10094b01514cb6bf27240763e83215717e3
Kumarjit Das <kumarjitdas43@gmail.com>
2024-11-18 00:56:14 +0530

    auth controller added

--- out/models/interface.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Gender = void 0;
-var Gender;
-(function (Gender) {
-    Gender["Male"] = "M";
-    Gender["Female"] = "F";
-    Gender["Other"] = "O";
-})(Gender || (exports.Gender = Gender = {}));
